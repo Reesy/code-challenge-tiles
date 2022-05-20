45fbd64287e5bbfb389f3fd8c9c8e184d7e37183
James Rees <James.Rees@outlook.com>
2022-05-20 10:03:17 +0100

    Initial styling of desktop size screen

--- src/components/TileComponent.tsx
+++ src/components/TileComponent.tsx
@@ -31,6 +31,15 @@ export default class TileComponent extends React.Component<TileComponentProps, T
 
         return (
             <div className="TileComponent-main">
+                <div className="one">1</div>
+                <div className="two">2</div>
+                <div className="three">3</div>
+                <div className="four">4</div>
+                <div className="five">5</div>
+                <div className="six">6</div>
+                <div className="seven">7</div>
+                <div className="eight">8</div>
+                <div className="nine">9</div>
             </div>
         )
     }
